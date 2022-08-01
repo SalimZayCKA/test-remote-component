7c16d460dcb816ae181a5731355ee026ed2a27db
SalimZayCKA <salim.zaywari@connekti.ca>
2022-08-01 11:23:27 -0400

    Added Helloworld from typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,6 @@
 import React from "react";
-import Button, { ButtonProps } from "@material-ui/core/Button";
+import { Title } from "./components/Title";
 
-type Modify<T, R> = Omit<T, keyof R> & R;
-
-export type CkaButtonProps = Modify<
-  ButtonProps,
-  {
-    color?: "primary" | "danger" | "default";
-    iconOnly?: boolean;
-    zeroPadding?: boolean;
-    forceClassName?: boolean;
-  }
->;
-
-const CkaButton: React.FC<CkaButtonProps> = ({
-  children,
-  color = "default",
-  variant = "contained",
-  disableElevation = true,
-  iconOnly = false,
-  zeroPadding,
-  className,
-  forceClassName = false,
-  ...restProps
-}) => {
-  return (
-    <Button
-      {...restProps}
-      variant={variant}
-      disableElevation={disableElevation}
-    >
-      {children}
-    </Button>
-  );
+export const App = ({ name = "World" }) => {
+  return <Title>Hello {name}!</Title>;
 };
-
-export default CkaButton;
